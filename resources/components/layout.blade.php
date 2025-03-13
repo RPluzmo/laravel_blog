507751c6dd40895d2c7fe2e0cb09ca46597d98f2
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-03-13 10:54:04 +0200

    blog

---
+++ resources/components/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? "LOG" }}</title>
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+</head>
+<body>
+    <x-navigation></x-navigation>
+    {{ $slot }}
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+  <x-slot:title>
+    Sākums
+  </x-slot:title>
+  <h1>Sveisksks LARAVEL</h1>
+</x-layout>
